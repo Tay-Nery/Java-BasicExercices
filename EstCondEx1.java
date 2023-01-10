04d19cd15afc71f5e8cdcf2888e95996e3180ec8
Taynara Nery <61115405+Tay-Nery@users.noreply.github.com>
2023-01-10 18:54:43 -0300

    Add files via upload
    Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.
    
    Make a program to read an integer, and then say if the number is negative or not.

---
+++ EstCondEx1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class EstCondEx1 {
+    public static void main(String[] args) {
+        int num;
+
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Escreva um numero");
+
+        num = ler.nextInt();
+
+        if (num >= 0) {
+            System.out.println("O numero é inteiro");
+        } else {
+                System.out.println("O numero é negativo");
+        }
+    }
+}
