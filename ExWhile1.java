2fc6afcb8d6ce5f6a4125a4b2d1d92abb3661c24
Taynara Nery <61115405+Tay-Nery@users.noreply.github.com>
2023-01-16 18:34:29 -0300

    Add files via upload

---
+++ ExWhile1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ExWhile1 {
+    public static void main(String[] args){
+       Scanner ler = new Scanner(System.in);
+       int senha = ler.nextInt();
+
+
+       while(senha != 2002){
+           System.out.println("Senha inválida");
+           senha = ler.nextInt();
+       }
+       System.out.println("Acesso permitido");
+       ler.close();
+    }
+}
