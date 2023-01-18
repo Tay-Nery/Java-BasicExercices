fb14b8fd9ae29a47a779fa448abdaabdd8813284
Taynara Nery <61115405+Tay-Nery@users.noreply.github.com>
2023-01-18 20:20:34 -0300

    Add files via upload
    Um Posto de combustíveis deseja determinar qual de seus produtos tem a preferência de seus clientes. Escreva
    um algoritmo para ler o tipo de combustível abastecido (codificado da seguinte forma: 1.Álcool 2.Gasolina 3.Diesel
    4.Fim). Caso o usuário informe um código inválido (fora da faixa de 1 a 4) deve ser solicitado um novo código (até
    que seja válido). O programa será encerrado quando o código informado for o número 4. Deve ser escrito a
    mensagem: "MUITO OBRIGADO" e a quantidade de clientes que abasteceram cada tipo de combustível, conforme
    exemplo.
    
    A gas station wants to determine which of its products are preferred by its customers. Write
    an algorithm to read the type of fuel supplied (coded as follows: 1.Alcohol 2.Gasoline 3.Diesel
    4.End). If the user enters an invalid code (outside the range 1 to 4), a new code must be requested. The program will be closed when the informed code is number 4. It must be written: "THANK YOU VERY MUCH" and the number of customers who supplied each type of fuel, as
    example.

---
+++ ExWhile3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ExWhile3 {
+    public static void main(String [] args){
+        Scanner ler = new Scanner(System.in);
+
+        int alcool = 0;
+        int gasolina = 0;
+        int diesel =0;
+
+        int tipo = ler.nextInt();
+
+        while (tipo != 4){
+            if(tipo ==1){
+                alcool = alcool+1;
+            } else if (tipo ==2) {
+                gasolina = gasolina +1;
+            } else if (tipo ==3) {
+                diesel = diesel +1;
+            }
+            tipo = ler.nextInt();
+        }
+        System.out.println("MUITO OBRIGADO");
+        System.out.println("Alcool: " + alcool);
+        System.out.println("Gasolina: " + gasolina);
+        System.out.println("Diesel: " + diesel);
+
+        ler.close();
+    }
+}
